8ae7bc3c92875ee2bf317b24b2c224e15bdd4587
Cliff Pan <cliffordpan.helenchen@gmail.com>
2024-06-09 03:06:06 -0700

    change to jenkins

--- src/main/java/me/hchome/example/service/DBService.java
+++ src/main/java/me/hchome/example/service/DBService.java
@@ -9,7 +9,7 @@ package me.hchome.example.service;
 public interface DBService {
 
 	/**
-	 * Reset database, all will reset by a sql script.
+	 * Reset database, all tables will reset by a sql script.
 	 */
 	void executeReset();
 }
